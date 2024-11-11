cf308311fc6734ff10e89fb8df8656a8c25c65d4
skidyuk8615 <chyu23072@g.nihon-u.ac.jp>
2024-11-11 09:22:15 +0900

    serverサイドプログラムの作成

---
+++ .netlify/functions-serve/hello/hello.mjs
@@ -0,0 +1,20 @@
+
+import {createRequire as ___nfyCreateRequire} from "module";
+import {fileURLToPath as ___nfyFileURLToPath} from "url";
+import {dirname as ___nfyPathDirname} from "path";
+let __filename=___nfyFileURLToPath(import.meta.url);
+let __dirname=___nfyPathDirname(___nfyFileURLToPath(import.meta.url));
+let require=___nfyCreateRequire(import.meta.url);
+
+
+// netlify/functions/hello.mjs
+var hello_default = async () => {
+  const data = {
+    message: "Hello, World!"
+  };
+  return new Response(JSON.stringify(data));
+};
+export {
+  hello_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsibmV0bGlmeS9mdW5jdGlvbnMvaGVsbG8ubWpzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJleHBvcnQgZGVmYXVsdCBhc3luYyAoKSA9PiB7XG4gIGNvbnN0IGRhdGEgPSB7XG4gICAgbWVzc2FnZTogXCJIZWxsbywgV29ybGQhXCIsXG4gIH07XG4gIHJldHVybiBuZXcgUmVzcG9uc2UoSlNPTi5zdHJpbmdpZnkoZGF0YSkpO1xufTsiXSwKICAibWFwcGluZ3MiOiAiOzs7Ozs7Ozs7O0FBQUEsSUFBTyxnQkFBUSxZQUFZO0FBQ3pCLFFBQU0sT0FBTztBQUFBLElBQ1gsU0FBUztBQUFBLEVBQ1g7QUFDQSxTQUFPLElBQUksU0FBUyxLQUFLLFVBQVUsSUFBSSxDQUFDO0FBQzFDOyIsCiAgIm5hbWVzIjogW10KfQo=
