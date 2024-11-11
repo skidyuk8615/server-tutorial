8d641b651a0b9123bc5c473f9aa34a6b0d45b453
skidyuk8615 <chyu23072@g.nihon-u.ac.jp>
2024-11-11 09:46:07 +0900

    reactとの連携

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/.netlify/functions/hello");
+      const data = await response.json();
+      setMessage(data.message);
+    })();
+  }, []);
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
